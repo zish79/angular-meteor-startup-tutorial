25f5a9f4d54f5e08fb08c78e64a51d2d9077de15
zaid iqbal <zish79@gmail.com>
2016-04-17 22:16:25 +0500

    Angular client structure

---
+++ client/main.js
@@ -0,0 +1,19 @@
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+
+angular.module('socially', [
+  angularMeteor
+])
+  .controller('PartiesListCtrl', function ($scope) {
+    'ngInject';
+    $scope.parties = [{
+      'name': 'Dubstep-Free Zone',
+      'description': 'Can we please just for an evening not listen to dubstep.'
+    }, {
+        'name': 'All dubstep all the time',
+        'description': 'Get it on!'
+      }, {
+        'name': 'Savage lounging',
+        'description': 'Leisure suit required. And only fiercest manners.'
+      }];
+  });
